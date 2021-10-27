3c8937cf6f263e5ba36bdc568c4381664c55bd0e
Fábio <fabioalmeida132@gmail.com>
2021-10-26 22:16:09 -0300

    inital commit, need ajusts

---
+++ src/components/SliderData.js
@@ -0,0 +1,22 @@
+export const SliderData = [
+  {
+    image: '/image4.jpg',
+    title: 'Telhas em poliéster reforçado com fibra de vidro',
+    subtitle: 'Qualidade e durabilidade com 10 anos de garantia.'
+  },
+  {
+    image: '/image3.jpg',
+    title: 'Telhas em poliéster reforçado',
+    subtitle: 'Qualidade e durabilidade com 10 anos de garantia.'
+  },
+  {
+    image: '/image2.jpg',
+    title: 'Telhas em poliéster reforçado com fibra de vidro',
+    subtitle: 'Qualidade e durabilidade com 10 anos de garantia.'
+  },
+  {
+    image: '/image1.jpg',
+    title: 'Telhas em poliéster reforçado com fibra de vidro',
+    subtitle: 'Qualidade e durabilidade com 10 anos de garantia.'
+  }
+];
